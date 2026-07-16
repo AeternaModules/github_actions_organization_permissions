a86958c6d5528320f770003e8e9cfe368dd2fbee
Daniel Allen <dan@aeternagroup.net>
2026-07-16 15:01:42 +0100

    Update to schema version v6.13.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "actions_organization_permissionses_id" {
   description = "Map of id values across all actions_organization_permissionses, keyed the same as var.actions_organization_permissionses"
-  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.id }
+  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "actions_organization_permissionses_allowed_actions" {
   description = "Map of allowed_actions values across all actions_organization_permissionses, keyed the same as var.actions_organization_permissionses"
-  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.allowed_actions }
+  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.allowed_actions if v.allowed_actions != null && length(v.allowed_actions) > 0 }
 }
 output "actions_organization_permissionses_allowed_actions_config" {
   description = "Map of allowed_actions_config values across all actions_organization_permissionses, keyed the same as var.actions_organization_permissionses"
-  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.allowed_actions_config }
+  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.allowed_actions_config if v.allowed_actions_config != null && length(v.allowed_actions_config) > 0 }
 }
 output "actions_organization_permissionses_enabled_repositories" {
   description = "Map of enabled_repositories values across all actions_organization_permissionses, keyed the same as var.actions_organization_permissionses"
-  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.enabled_repositories }
+  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.enabled_repositories if v.enabled_repositories != null && length(v.enabled_repositories) > 0 }
 }
 output "actions_organization_permissionses_enabled_repositories_config" {
   description = "Map of enabled_repositories_config values across all actions_organization_permissionses, keyed the same as var.actions_organization_permissionses"
-  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.enabled_repositories_config }
+  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.enabled_repositories_config if v.enabled_repositories_config != null && length(v.enabled_repositories_config) > 0 }
 }
 output "actions_organization_permissionses_sha_pinning_required" {
   description = "Map of sha_pinning_required values across all actions_organization_permissionses, keyed the same as var.actions_organization_permissionses"
-  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.sha_pinning_required }
+  value       = { for k, v in github_actions_organization_permissions.actions_organization_permissionses : k => v.sha_pinning_required if v.sha_pinning_required != null }
 }
 
